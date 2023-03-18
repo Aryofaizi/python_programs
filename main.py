4c9978efa9843e8427a590cdd4bb0fc3adbba2c0
aryo faizi <aryofaizi@gmail.com>
2023-03-18 19:29:54 +0330

    enter key binded to the ent_fahrenheit of the window and ttk imported for better appearance,

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import tkinter as tk
-from tkinter import E,W,N,S
+from tkinter import ttk
 window = tk.Tk()
 
 window.title("This is my app")
 
-
 def fahrenheit_to_celcius(faren):
     return (5/9) * (faren-32)
 
-lbl_fahrenheit = tk.Label(
+lbl_fahrenheit = ttk.Label(
     master=window,
     text="Fahrenheit:",
 )
-ent_fahrenheit = tk.Entry(
+ent_fahrenheit = ttk.Entry(
     master=window,
     width=55,
 )
 
-def btn_command():
+def btn_command(*args):
     try:
         assert not len(ent_fahrenheit.get()) == 0
     except AssertionError:
@@ -31,17 +30,17 @@ def btn_command():
         else:
             lbl_result["text"] = celcius
 
-btn_calc = tk.Button(
+btn_calc = ttk.Button(
     master=window,
     text="Calc",
     width=9,
     command=btn_command
 )
-lbl_celsius = tk.Label(
+lbl_celsius = ttk.Label(
     master=window,
     text="Celsius",
 )
-lbl_result = tk.Label(
+lbl_result = ttk.Label(
     master=window,
     text="Result will be shown here...",
 )
@@ -50,6 +49,8 @@ ent_fahrenheit.grid(row=0,column=1,padx=10,pady=(10,5))
 btn_calc.grid(row=0,column=2,padx=10,pady=(10,5))
 lbl_celsius.grid(row=1,column=0,padx=10,pady=(10,20))
 lbl_result.grid(row=1,column=1,padx=10,pady=(10,20))
+
+window.bind("<Return>",btn_command)
 window.mainloop()
 
 
